7552c47110f84ae99766602455a1d2fadffe2618
Rustam <rustam.dranitsyn@mail.ru>
2020-11-28 14:50:57 +0600

    123

--- app/js/main.js
+++ app/js/main.js
@@ -82,7 +82,7 @@ window.onload = function () {
       }, timeout);
    }
 }
-// // Кнопка наверх
+// Кнопка наверх
 // function scrollTo(to, duration = 700) {
 //    const
 //       element = document.scrollingElement || document.documentElement,
@@ -116,3 +116,100 @@ window.onload = function () {
 //       scrollTo(0, 400);
 //    }
 // });
+
+
+
+
+const itemHead = document.querySelectorAll('.tab-head');
+const tabItem = document.querySelectorAll('.tab-body__item');
+function oneTab1() {
+   const id1 = document.querySelector('#id1');
+   const tab1 = document.querySelector('#tab1');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id1.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab1.classList.add('active-block');
+}
+
+function oneTab2() {
+   const id2 = document.querySelector('#id2');
+   const tab2 = document.querySelector('#tab2');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id2.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab2.classList.add('active-block');
+}
+
+function oneTab3() {
+   const id3 = document.querySelector('#id3');
+   const tab3 = document.querySelector('#tab3');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id3.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab3.classList.add('active-block');
+}
+
+function oneTab4() {
+   const id4 = document.querySelector('#id4');
+   const tab4 = document.querySelector('#tab4');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id4.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab4.classList.add('active-block');
+}
+
+function oneTab5() {
+   const id5 = document.querySelector('#id5');
+   const tab5 = document.querySelector('#tab5');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id5.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab5.classList.add('active-block');
+}
+
+function oneTab6() {
+   const id6 = document.querySelector('#id6');
+   const tab6 = document.querySelector('#tab6');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id6.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab6.classList.add('active-block');
+}
+
+function oneTab7() {
+   const id7 = document.querySelector('#id7');
+   const tab7 = document.querySelector('#tab7');
+   for (let i = 0; i < itemHead.length; i++) {
+      itemHead[i].classList.remove('active');
+   }
+   id7.classList.add('active');
+   for (let i = 0; i < tabItem.length; i++) {
+      tabItem[i].classList.remove('active-block');
+   }
+   tab7.classList.add('active-block');
+}
+
