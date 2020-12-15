2d28fd13c38ce4f8f1a9bcc3a5f967866de1ce20
Rustam <rustam.dranitsyn@mail.ru>
2020-12-15 22:36:40 +0600

    qwe

--- app/js/main.js
+++ app/js/main.js
@@ -215,4 +215,29 @@ function oneTab7() {
 
 
 
-// разворачивание прайс-лист
+var t, a;
+function example(e){
+clearTimeout(t);
+var w = e.width;
+if (a) {
+t = setInterval(function () {
+if (w <= 450) clearTimeout(t); // Стили на уменьшение
+e.style.cursor = 'zoom-in';
+e.style.borderRadius = '1px';
+e.style.boxShadow = '2px 2px 5px #fff'
+e.width = w--;
+}, 5);
+}
+else {
+t = setInterval(function () {
+if (w >= 650) clearTimeout(t);
+// Стили на увеличение
+e.style.cursor = 'zoom-out';
+e.style.borderRadius = '5px';
+e.style.position = 'absolute';
+e.style.boxShadow = '2px 2px 5px #888'
+e.width = w++;
+}, 1);
+}
+a = !a;
+}
