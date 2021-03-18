410eb476e7aa9da14852fa7ce7f22f512a8a5df6
Rustam <rustam.dranitsyn@mail.ru>
2021-03-18 21:34:33 +0600

    slick slider

---
+++ app/js/slick.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('.slider').slick({
+        infinite: true,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 3,
+        autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
